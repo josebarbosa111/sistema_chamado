22779883215c4144243a39089a524c07676b8de9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-11 20:48:39 -0300

    muita coisa, nicolas é lindo

--- php/insert_user.php
+++ php/insert_user.php
@@ -12,11 +12,11 @@ $senha = $_POST['senha'];
 
 $sql = "INSERT INTO `tb_user` VALUES (NULL, '$nome', '$email', '$setor', '$senha');";
 
-if ($conn->query($sql) === TRUE) {
+if ($conexao->query($sql) === TRUE) {
   echo "<script>alert('Usuario cadastrado com sucesso'); history.back() </script>";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conexao->error;
 }
 
-$conn->close();
+$conexao->close();
 ?>
